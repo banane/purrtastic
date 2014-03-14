1293cd8adb051b649a2831f0d6e9bc81f0365056
Anna Billstrom <banane@gmail.com>
2014-03-14 01:34:15 -0700

    animation kinda working, not very ... fluid

--- PetToGive/ViewController.h
+++ PetToGive/ViewController.h
@@ -12,15 +12,21 @@
     IBOutlet UIImageView *petHand;
     UIPanGestureRecognizer *panRecognizer;
     IBOutlet UIImageView *petPhoto;
-
-    
-    
+    int heartCounter;
+    NSArray *heartXPositions;
+    int petCount;
+    int toggle;
 }
 
 @property (nonatomic, strong) IBOutlet UIImageView *petHand;
 @property (nonatomic, strong) UIPanGestureRecognizer *panRecognizer;
 @property (nonatomic, strong) IBOutlet UIImageView *petPhoto;
+@property (nonatomic, strong) NSArray *heartXPositions;
 
 - (void)petAction;
+- (void)animateHearts;
+- (UIView *)addHeart;
+- (void)ZigZag:(UIView *)heart ;
+- (void)fadeOpacity:(UIView *)heart;
 
 @end
--- PetToGive/ViewController.m
+++ PetToGive/ViewController.m
@@ -14,25 +14,117 @@
 
 @implementation ViewController
 
-@synthesize petHand, panRecognizer, petPhoto;
+@synthesize petHand, panRecognizer, petPhoto, heartXPositions;
 
 -(void)petAction{
     NSLog(@"pet action");
     petHand.hidden = YES;
+
+    CGPoint translatedPoint = [(UIPanGestureRecognizer*)panRecognizer translationInView:petPhoto];
+    NSLog(@"%f, %f", translatedPoint.x, translatedPoint.y);
+    if(abs(translatedPoint.x) > 20 || abs(translatedPoint.y) > 20){
+        if(petCount == 4){
+            NSLog(@"valid pet");
+            [self animateHearts];
+            petCount = 0;
+        } else {
+            petCount += 1;
+        }
+    }
 }
 
 - (void)viewDidLoad
 {
+    petCount = 0;
+    petHand.hidden = NO;
+    heartCounter = 1;
+    
+    heartXPositions = [[NSArray alloc] initWithObjects:
+                       [NSNumber numberWithInteger:150],
+                       [NSNumber numberWithInteger:140],
+                       [NSNumber numberWithInteger:210],
+                       [NSNumber numberWithInteger:230],
+                       [NSNumber numberWithInteger:190],
+                       [NSNumber numberWithInteger:260],
+                       [NSNumber numberWithInteger:200],
+                       [NSNumber numberWithInteger:130], nil];
+
     [super viewDidLoad];
 	panRecognizer = [[UIPanGestureRecognizer alloc]
                        initWithTarget:self action:@selector(petAction)];
+    
     [petPhoto addGestureRecognizer:panRecognizer];
 }
+
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)animateHearts{
+    UIView *heart = [self addHeart];
+    
+    NSLog(@"heart x: %f, heart y: %f", heart.frame.origin.x, heart.frame.origin.y);
+    [self ZigZag:heart];
+    NSLog(@"heart x: %f, heart y: %f", heart.frame.origin.x, heart.frame.origin.y);
+    
+    
+}
+- (void)fadeOpacity:(UIView *)heart{
+    float targetAlpha = heart.alpha - 0.2;
+    
+    [UIView animateWithDuration:0.2
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{heart.alpha = targetAlpha;}
+                     completion:^(BOOL finished){
+                         [self ZigZag:heart];
+                     }];
+}
+
+- (UIView *)addHeart{
+    // create 1-8 hearts (later include stars)
+    int x = [[heartXPositions objectAtIndex:(heartCounter-1)] intValue];
+    UIImageView *heart=[[UIImageView alloc] initWithFrame:CGRectMake(x, 300, 40, 40)];
+    heart.image = [UIImage imageNamed:[NSString stringWithFormat:@"heart%d", heartCounter]];
+    [self.view addSubview:heart];
+    
+    // reset heartcounter values
+    if(heartCounter == 8){
+        heartCounter = 1;
+    } else {
+        heartCounter += 1;
+    }
+    return heart;
+}
+
+
+-(void)ZigZag:(UIView *)heart{
+    toggle ^= 1;
+    
+    CGRect newFrame = heart.frame;
+    if(toggle == 0){
+        newFrame.origin.x = heart.frame.origin.x - 50;
+    } else {
+        newFrame.origin.x = heart.frame.origin.x + 50;
+    }
+    newFrame.origin.y = heart.frame.origin.y - 50;
+    
+    
+    [UIView animateWithDuration:0.5
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         heart.frame = newFrame;
+                     }
+                     completion:^(BOOL finished){
+                         if(heart.frame.origin.y > 0){  // do until top of window
+                             [self fadeOpacity:heart];
+                         }
+                     }
+     ];
 }
 
 
